d2ec8451575e3b8ec3a88c2c80eca69564aa1845
Harrison Smith <harrisonws320@gmail.com>
2023-10-03 10:37:26 -0500

    additions from other solution

--- MyDungeon/MyDungeonApp/Program.cs
+++ MyDungeon/MyDungeonApp/Program.cs
@@ -3,9 +3,33 @@ namespace MyDungeonApp
 {
     internal class Program
     {
+        private static string[] demonNames = { "Stavrogin", "Pope Bacon X", "Zarathustra", "Pynchon", "Milton", /* Add more demon names */ };
+
+        private static Room[] rooms = new Room[4];
+
         static void Main(string[] args)
         {
-            Console.WriteLine("VOID");
+            // Generate seven random rooms
+            GenerateRandomRooms();
+
+            // Display the rooms and associated demons
+            for (int i = 0; i < rooms.Length; i++)
+            {
+                Console.WriteLine($"Room {i + 1}: {rooms[i].DemonName}");
+            }
+        }
+
+        static void GenerateRandomRooms()
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < rooms.Length; i++)
+            {
+                Room room = new Room();
+                int randomIndex = random.Next(0, demonNames.Length);
+                room.DemonName = demonNames[randomIndex];
+                rooms[i] = room;
+            }
         }
     }
 }
---
+++ MyDungeon/MyDungeonLibrary/Room.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDungeonLibrary
+{
+    public class Room
+    {
+        public string DemonName { get; set; }
+
+        public Room()
+        {
+            DemonName = string.Empty;
+        }
+    }
+}
