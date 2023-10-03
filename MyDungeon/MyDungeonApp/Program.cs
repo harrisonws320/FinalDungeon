0397a8cc6c8bbda187ab77b317b3f6bf96722f29
Harrison Smith <harrisonws320@gmail.com>
2023-10-03 00:57:54 -0500

    ?

--- MyDungeon/MyDungeonApp/Program.cs
+++ MyDungeon/MyDungeonApp/Program.cs
@@ -5,7 +5,7 @@ namespace MyDungeonApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("VOID");
         }
     }
 }
